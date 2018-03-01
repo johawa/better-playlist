498b3547d2b3c22b75d392e898d9193b0ba8e4d5
timebasher <johannes.walenta@googlemail.com>
2018-03-01 14:18:53 +0100

    load album covers

--- better-playlists/src/App.js
+++ better-playlists/src/App.js
@@ -35,40 +35,19 @@ class Scrollable extends Component {
     return (
       <div id="scrollable">
         <ul id="items">
-          <li>A</li>
-          <li>B</li>
-          <li>C</li>
-          <li>D</li>
-          <li>E</li>
-          <li>F</li>
-          <li>G</li>
-          <li>H</li>
-          <li>I</li>
-          <li>J</li>
-          <li>K</li>
-          <li>L</li>
-          <li>M</li>
-          <li>N</li>
-          <li>O</li>
-          <li>P</li>
-          <li>Q</li>
-          <li>R</li>
-          <li>S</li>
-          <li>T</li>
-          <li>U</li>
-          <li>V</li>
-          <li>W</li>
-          <li>X</li>
-          <li>Y</li>
-          <li>Z</li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>    
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>           
         </ul>
       </div>
-
-
     );
-
-
-
 
   }
 }
@@ -93,19 +72,65 @@ class App extends Component {
       serverData: { email: null },
     }
   }
+
+
   componentDidMount() {
     let parsed = queryString.parse(window.location.search)
     let accessToken = parsed.access_token
 
-    fetch('https://api.spotify.com/v1/me', {
+
+    fetch('https://api.spotify.com/v1/search?q=michael+jackson&type=album', {
       headers: { 'Authorization': 'Bearer ' + accessToken }
-    }).then(response => response.json())
-    /*   .then(data => this.setState({ serverData: { email: data.email } })) */
+    })
+      .then(response => response.json())
+      .then(data => {
+        const uris = data.albums.items.map(item => {
+          return item.id;
+        }).slice(0, 10);
+        const tasks = uris.map(id => fetch('https://api.spotify.com/v1/albums/' + id, {
+          headers: { 'Authorization': 'Bearer ' + accessToken }
+        }))
+        return Promise.all(tasks);
+      })
+      .then(responses => Promise.all(responses.map(r => r.json())))
+      .then(albums => Promise.all(albums.map(album => fetch(album.images[0].url))))
+      .then(responses => Promise.all(responses.map(r => r.blob())))
+      .then(blobs => blobs.map(blob => URL.createObjectURL(blob)))
+      .then(urls => {
+        return Promise.all(urls.map(url => {
+          return new Promise(resolve => {
+            const image = new Image();
+            image.addEventListener('load', () => {
+              resolve(image);
+            });
+            image.src = url;
+          })
+        }));
+      })
+      .then(images => {
+        let imgArr = [];
+        images.forEach(image => {
+          imgArr.push(image.outerHTML);
+        });
+        const ul = document.getElementById("items");
+        const li = Array.from(ul.children);
+        for (let i = 0; i < li.length; i++) {
+          li[i].style.zIndex = [i]*-1;
+          li[i].innerHTML = imgArr[i];
+        };
+      });
+
+
+
+
+
+
+
 
   }
 
   render() {
-    let email = this.state.serverData.email 
+    let email = this.state.serverData.email
     return (
       < div >
         <h1>{email}</h1>
@@ -122,3 +147,5 @@ class App extends Component {
 export default App;
 
 
+    /* 'https://api.spotify.com/v1/me' */
+ /*   .then(data => this.setState({serverData: {email: data.email } })) */
