8bc6414be78c3952bb70035185a0e95023a69b20
timebasher <johannes.walenta@googlemail.com>
2018-03-02 11:11:42 +0100

    search button and input

--- better-playlists/src/App.js
+++ better-playlists/src/App.js
@@ -22,9 +22,19 @@ class Filter extends Component {
     return (
       <div style={{ ...defaultStyle }}>
         <img />
-        <input type="text" />
-
+        <input type="text" onKeyUp={event => this.props.onTextChange(event.target.value)} />
       </div>
+    );
+  }
+}
+
+class Search extends Component {
+  render() {
+    return (
+      <form id="search-form">
+        <input type="text" id="query" value="" class="form-control" placeholder="Type an Artist Name" />
+        <input type="submit" id="search" class="btn btn-primary" value="Search" />
+      </form>
     );
   }
 }
@@ -72,6 +82,9 @@ class App extends Component {
     super();
     this.state = {
       serverData: { email: null },
+      filterString: 'oasis',
+      albumNames: []
+
     }
   }
 
@@ -79,64 +92,14 @@ class App extends Component {
   componentDidMount() {
 
 
-    //Fetch Spotify Data
+
     let parsed = queryString.parse(window.location.search)
     let accessToken = parsed.access_token
 
-
-    fetch('https://api.spotify.com/v1/search?q=nirvana&type=album', {
-      headers: { 'Authorization': 'Bearer ' + accessToken }
-    })
-      .then(response => response.json())
-      .then(data => {
-        const uris = data.albums.items.map(item => {
-          return item.id;
-        }).slice(0, 10);
-        const tasks = uris.map(id => fetch('https://api.spotify.com/v1/albums/' + id, {
-          headers: { 'Authorization': 'Bearer ' + accessToken }
-        }))
-        return Promise.all(tasks);
-      })
-      .then(responses => Promise.all(responses.map(r => r.json())))
-      .then(albums => Promise.all(albums.map(album => fetch(album.images[0].url))))
-      .then(responses => Promise.all(responses.map(r => r.blob())))
-      .then(blobs => blobs.map(blob => URL.createObjectURL(blob)))
-      .then(urls => {
-        return Promise.all(urls.map(url => {
-          return new Promise(resolve => {
-            const image = new Image();
-            image.addEventListener('load', () => {
-              resolve(image);
-            });
-            image.src = url;
-          })
-        }));
-      })
-      .then(images => {
-        let imgArr = [];
-        images.forEach(image => {
-          imgArr.push(image.outerHTML);
-        });
-        const ul = document.getElementById("items");
-        const li = Array.from(ul.children);
-        for (let i = 0; i < li.length; i++) {
-          li[i].style.zIndex = 100 + ([i] * -1);
-          li[i].innerHTML = imgArr[i];
-        };
-      });
-
-
-
-
     //Scroll Animation
-
 
     let scrollable = document.getElementById("scrollable")
     let items = document.getElementById("items")
-
-
-
-
 
     function scrollMiddleWare(inertia = 0.8) {
       const delta = {
@@ -203,6 +166,25 @@ class App extends Component {
     })
 
 
+    //Get Email Name 
+
+    fetch('https://api.spotify.com/v1/me', {
+      headers: { 'Authorization': 'Bearer ' + accessToken }
+    })
+      .then(response => response.json())
+      /*  .then(data => console.log(data)) */
+      .then(data => this.setState({ serverData: { email: data.email } }))
+
+
+    fetch('https://api.spotify.com/v1/search?q=oasis&type=album', {
+      headers: { 'Authorization': 'Bearer ' + accessToken }
+    })
+      .then(response => response.json())
+      .then(data => {
+        const names = data.albums.items;
+        console.log(names);
+      })
+
 
 
 
@@ -218,13 +200,70 @@ class App extends Component {
 
   render() {
     let email = this.state.serverData.email
+
+
+    //Fetch Spotify Data   
+    let parsed = queryString.parse(window.location.search)
+    let accessToken = parsed.access_token
+    let search = this.state.filterString
+
+    fetch('https://api.spotify.com/v1/search?q=' + search + '&type=album', {
+      headers: { 'Authorization': 'Bearer ' + accessToken }
+    })
+      .then(response => response.json())
+      .then(data => {
+        const uris = data.albums.items.map(item => {
+          return item.id;
+        }).slice(0, 10);
+        const tasks = uris.map(id => fetch('https://api.spotify.com/v1/albums/' + id, {
+          headers: { 'Authorization': 'Bearer ' + accessToken }
+        }))
+        return Promise.all(tasks);
+      })
+      .then(responses => Promise.all(responses.map(r => r.json())))
+      .then(albums => Promise.all(albums.map(album => fetch(album.images[0].url))))
+      .then(responses => Promise.all(responses.map(r => r.blob())))
+      .then(blobs => blobs.map(blob => URL.createObjectURL(blob)))
+      .then(urls => {
+        return Promise.all(urls.map(url => {
+          return new Promise(resolve => {
+            const image = new Image();
+            image.addEventListener('load', () => {
+              resolve(image);
+            });
+            image.src = url;
+          })
+        }));
+      })
+      .then(images => {
+        let imgArr = [];
+        images.forEach(image => {
+          imgArr.push(image.outerHTML);
+        });
+        const ul = document.getElementById("items");
+        const li = Array.from(ul.children);
+        for (let i = 0; i < li.length; i++) {
+          li[i].style.zIndex = 100 + ([i] * -1);
+          li[i].innerHTML = imgArr[i];
+        };
+      });
+
+
+
+
     return (
       < div >
         <h1>{email}</h1>
+        <h2>{search}</h2>
         <Aggregate />
         <Aggregate />
-        <Filter />
-        <Scrollable />
+
+        <Filter onTextChange={text =>
+          this.setState({ filterString: text },
+          )} />
+
+        <Scrollable/>
+        <Search/>
         <Album />
       </div >
     );
